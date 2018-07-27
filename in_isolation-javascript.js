3edda1f377e43b3d5c980db59eaaf5bca2ba47fd
nosepoke <ellen.rmb@gmail.com>
2018-07-27 13:39:56 -0500

    Started timer work
    This is a heck of a lot harder than the inventory system. Or maybe it's not, but I decided not to brute-force the timer like I did the inventory system. :)

--- in_isolation-javascript.js
+++ in_isolation-javascript.js
@@ -1,3 +1,13 @@
 //$('#ui-bar').remove();
 //$(document.head).find('#style-ui-bar').remove();
 config.addVisitedLinkClass = true;
+
+
+Config.navigation.override = function (d) {
+    if (State.variables.countDown) {
+        if ((State.variables.countDown) < 100) {
+            return 'GameOver';
+        }
+    }
+    return false;
+};
